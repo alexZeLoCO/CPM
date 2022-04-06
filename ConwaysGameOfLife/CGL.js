476927cfd60aa404004688a0c27855b2acbd85a4
alexZeLoCO <alejandro.rod.lop@gmail.com>
2022-04-06 22:54:07 +0200

    Added Factory method, some templates, more parameters and some output formatting

--- ConwaysGameOfLife/CGL.js
+++ ConwaysGameOfLife/CGL.js
@@ -1,33 +1,117 @@
-class CGL {
-
-    constructor (string) {
-        this.start(string);
+class Factory {
+    static newBlinker () {
+        return [[0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0]];
     }
 
-    init (string) {
+    static newBeacon () {
+        return [[0, 0, 0, 0, 0, 0],
+                [0, 1, 1, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0]];
+    }
+
+    static newToad () {
+        return [[0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 1, 1, 0],
+                [0, 1, 1, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0]];
+    }
+
+    static newGlider () {
+        return [[0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0],
+                [0, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0]];
+    }
+
+    static newPentomino () {
+        return [[0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 1, 0],
+                [0, 1, 1, 0, 0],
+                [0, 0, 0, 0, 0]];
+    }
+}
+
+class Logic {
+    static sleep (ms) {
+        return new Promise (resolve => setTimeout(resolve, ms));
+    }
+
+    static numbers (n) {
+        return Math.floor(Math.log10(n)+1);
+    }
+}
+
+class CGL {
+
+    constructor (string, iters, size, boolean) {
+        this.iters = iters;
+        this.init(string, boolean, size);
+        this.start(iters);
+    }
+
+    init (string, boolean, size) {
         this.board = [];
         this.aux_board = [];
+        this.previous = [];
+        for (let i = 0 ; i < size ; i++) {
+            this.board[i] = [];
+            for (let j = 0 ; j < size ; j++) {
+                if (boolean) {
+                    this.board[i][j] = (Math.random() > 0.5) ? 1 : 0;
+                } else {
+                    this.board[i][j] = 0;
+                } 
+            }
+        }
         switch (string) {
-            case(""):
-                for (let i = 0 ; i < 5 ; i++) {
+            case (""):
+                for (let i = 0 ; i < this.board.length ; i++) {
                     this.board[i] = [];
-                    for (let j = 0 ; j < 5 ; j++) {
+                    for (let j = 0 ; j < this.board[i].length ; j++) {
                         this.board[i][j] = (Math.random() > 0.5) ? 1 : 0;
                     }
                 }
                 break;
-            case("Blinker"):
-                this.board = [  [0, 0, 0, 0, 0],
-                            [0, 0, 1, 0, 0],
-                            [0, 0, 1, 0, 0],
-                            [0, 0, 1, 0, 0],
-                            [0, 0, 0, 0, 0]];
-
+            case ("Blinker"):
+                this.merge(Factory.newBlinker());
+                break;
+            case ("Beacon"):
+                this.merge(Factory.newBeacon());
+                break;
+            case ("Toad"):
+                this.merge(Factory.newToad());
+                break;
+            case ("Glider"):
+                this.merge(Factory.newGlider());
+                break;
+            case ("Pentomino"):
+                this.merge(Factory.newPentomino());
+                break;
         }
-        for (let i = 0 ; i < 5 ; i++) {
+        for (let i = 0 ; i < this.board.length ; i++) {
             this.aux_board[i] = [];
-            for (let j = 0 ; j < 5 ; j++) {
+            for (let j = 0 ; j < this.board[i].length ; j++) {
                 this.aux_board[i][j] = this.board[i][j];
+            }
+        }
+        this.updatePrevious();
+    }
+
+    merge (m) {
+        for (let i = 0 ; i < m.length ; i++) {
+            for (let j = 0 ; j < m[i].length ; j++) {
+                this.board[this.board.length/2+i][this.board[0].length/2+j] = m[i][j];
             }
         }
     }
@@ -73,35 +157,81 @@ class CGL {
     }
 
     update () {
-        for (let i = 0 ; i < 5 ; i++) {
-            for (let j = 0 ; j < 5; j++) {
+        for (let i = 0 ; i < this.board.length ; i++) {
+            for (let j = 0 ; j < this.board[i].length ; j++) {
                 this.aux_board[i][j] = this.updateCell(i,j);
             }
         }
-        for (let i = 0 ; i < 5 ; i++) {
-            for (let j = 0 ; j < 5 ; j++) {
+        for (let i = 0 ; i < this.board.length ; i++) {
+            for (let j = 0 ; j < this.board[i].length ; j++) {
                 this.board[i][j] = this.aux_board[i][j];
             }
         }
     }
 
     showBoard () {
-        for (let i = 0 ; i < 5; i++) {
-            for (let j = 0 ; j < 5; j++) {
+        for (let i = 0 ; i < this.board.length ; i++) {
+            for (let j = 0 ; j < this.board[i].length ; j++) {
                 process.stdout.write(this.board[i][j] + " ");
             }
             console.log();
         }
     }
 
-    start(string) {
-        this.init(string);
-        for (let i = 0 ; i < 5; i++) {
-            this.showBoard();
-            this.update();
-            console.log("#########")
+    separator (idx) {
+        let off = Logic.numbers(idx)%2;
+        for (let i = 0 ; i < this.board[0].length-Logic.numbers(idx)/2-off; i++) {
+                process.stdout.write("#");
         }
+        process.stdout.write((idx+1) + "");
+        for (let i = 0 ; i < this.board[0].length-(Logic.numbers(idx)-Logic.numbers(idx)/2)-1; i++) {
+                process.stdout.write("#");
+        }
+        console.log();
+    }
+
+    updatePrevious () {
+        for (let i = 0 ; i < this.board.length ; i++) {
+            this.previous[i] = [];
+            for (let j = 0 ; j < this.board[i].length; j++) {
+                this.previous[i][j] = this.board[i][j];
+            }
+        }
+    }
+
+    differentBoards() {
+        for (let i = 0 ; i < this.board.length ; i++) {
+            for (let j = 0 ; j < this.board.length ; j++) {
+                if (this.board[i][j] != this.previous[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false; 
+    }
+
+    async start(iters) {
+        let i = 1;
+        do {
+            this.separator(i++);
+            this.showBoard();
+            this.updatePrevious();
+            this.update();
+            await Logic.sleep(50);
+        } while (iters--!=0 && this.differentBoards());
+/*
+        for (let i = 0 ; i < this.iters ; i++) {
+            this.separator(i);
+            this.showBoard();
+            this.updatePrevious();
+            this.update();
+            await Logic.sleep(100);
+            if (this.equalBoards()) {
+                break;
+            }
+        }
+        */
     }
 }
 
-CGL = new CGL("");
+CGL = new CGL("Pentomino", 1200, 100, false);
