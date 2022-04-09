24d886db8878a812a9343cce020bdb2139bfedba
alexZeLoCO <alejandro.rod.lop@gmail.com>
2022-04-09 13:48:08 +0200

    Added an html-working ConwaysGameOfLife

---
+++ ConwaysGameOfLife/CGL_no_OOP/CGL_no_OOP.js
@@ -0,0 +1,206 @@
+class Factory {
+    // Oscillators 
+    static newBlinker () {
+        return [[0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0]];
+    }
+
+    static newBeacon () {
+        return [[0, 0, 0, 0, 0, 0],
+                [0, 1, 1, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0]];
+    }
+
+    static newToad () {
+        return [[0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 1, 1, 0],
+                [0, 1, 1, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0]];
+    }
+
+    // Spaceships
+    static newGlider () {
+        return [[0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0],
+                [0, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0]];
+    }
+
+    // Methuselahs
+    // FIXME: None of these seem to work. I may need a matrix bigger than my screen for them to become stable.
+    static newPentomino () {
+        return [[0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 1, 0],
+                [0, 1, 1, 0, 0],
+                [0, 0, 0, 0, 0]];
+    }
+
+    static newWikipedia () {
+        return [[0, 0, 0, 0, 0],
+                [0, 1, 1, 0, 0],
+                [0, 0, 1, 1, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0]];
+    }
+
+    static newDiehard () {
+        return [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+    }
+
+    static newDiamond() {
+        return [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+    }
+}
+
+class Logic {
+    static sleep (ms) {
+        return new Promise (resolve => setTimeout(resolve, ms));
+    }
+
+    static numbers (n) {
+        return Math.floor(Math.log10(n)+1);
+    }
+}
+
+function newTemplate (name) {
+    switch (name) {
+        case 'Blinker': 
+        return Factory.newBlinker(); break;
+        case 'Beacon':
+            return Factory.newBeacon(); break;
+        case 'Toad':
+            return Factory.newToad(); break;
+        case 'Glider':
+            return Factory.newGlider(); break;
+        case 'Pentomino': 
+            return Factory.newPentomino(); break;
+        case 'Wikipedia':
+            return Factory.newWikipedia(); break;
+    }
+}
+
+function getPrintout (board) {
+    let out = "";
+    for (let i = 0 ; i < 5 ; i++) {
+        for (let j = 0 ; j < 5 ; j++) {
+            out+=board[i][j] + " ";
+        }
+        out+="<br>";
+    }
+    return out;
+}
+
+function showBoard (board) {
+    document.getElementById("output").innerHTML =
+        document.getElementById("output").innerHTML + "<br>" +
+        getPrintout(board);
+}
+
+function output (text) {
+    document.getElementById("output").innerHTML =
+        document.getElementById("output").innerHTML + text;
+}
+
+function neighbors (board, row, col) {
+    let count = 0;
+    if (row > 0) {
+        count=board[row-1][col];   // up
+        if (col > 0) {
+            count+=board[row-1][col-1]; // up-left
+        }
+        if (col < board[0].length-1) {
+            count+=board[row-1][col+1]; // up-right
+        }
+    }
+    if (row < board.length-1) {
+        count+=board[row+1][col];   // bottom
+        if (col > 0) {
+            count+=board[row+1][col-1]; // bottom-left
+        }
+        if (col < board[0].length-1) {
+            count+=board[row+1][col+1]; // bottom-right
+        }
+    }
+    if (col > 0) {
+        count+=board[row][col-1];   // left
+    }
+    if (col < board[0].length-1) {
+        count+=board[row][col+1];   // right
+    }
+    return count;
+}
+
+function update (board, aux_board) {
+    for (let i = 0 ; i < board.length ; i++) {
+        for (let j = 0 ; j < board[i].length ; j++) {
+            aux_board[i][j] = updateCell(board, i,j);
+        }
+    }
+    return aux_board;
+}
+
+function sync (board, aux_board) {
+    for (let i = 0 ; i < board.length ; i++) {
+        for (let j = 0 ; j < board[i].length ; j++) {
+            board[i][j] = aux_board[i][j];
+        }
+    }
+}
+
+function updateCell(board, row, col) {
+    const n = neighbors(board, row, col);
+    if (n !== 2 && n!==3){
+        return 0;       // Any alive cell with other 2 or 3 neighbors dies
+    }
+    if (board[row][col] === 0 && n===3) {
+        return 1;       // Any dead cell with thre 3 neighbors becomes a live cell
+    }
+    return board[row][col];
+}
+
+function separator (idx, size) {
+    let off = Logic.numbers(idx)%2;
+    let str = "";
+    for (let i = 0 ; i < size-Logic.numbers(idx)/2-off-1 ; i++) {
+        str+="#";
+    }
+    str+=idx;
+    for (let i = 0 ; i < size-(Logic.numbers(idx)-(Logic.numbers(idx)/2-off))-1 ; i++) {
+        str+="#";
+    }
+    output(str);
+}
+
+function play (name, iters) {
+    let board = newTemplate(name);
+    let aux_board = newTemplate(name);
+    for (let i = 0 ; i < iters ; i++) {
+        showBoard(board);
+        separator(i+1, board[0].length);
+        sync(board, update(board, aux_board));
+    }
+}
