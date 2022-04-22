b0c06331ead3bbce4145044bbb9a3291112b81a6
Alex <alejandro.rod.lop@gmail.com>
2022-04-22 16:06:28 +0200

    Added S9

--- HTML/9/EjercicioIntl_ACompletar/public_html/code/Pedido.js
+++ HTML/9/EjercicioIntl_ACompletar/public_html/code/Pedido.js
@@ -10,3 +10,24 @@
  *  currencyFormat
  *  dateFormat
 */
+
+function writeNumber (input, output) {
+	if (document.getElementById(input).value) {
+		var n =new Number(document.getElementById(input).value);
+		if (n) {
+			document.getElementById(output).innerHTML = "( " + numberFormat.format(n) + " )";
+		} else {
+			document.getElementById(output).innerHTML = "(  )";
+		}
+	} else {
+		document.getElementById(output).innerHTML = "";
+	}
+}
+
+function calc (nScrews, idScrewWeight, idScrewPrice, idOneScrewWeight, idTotalPrice, idDate) {
+	if (document.getElementById(nScrews).value &&
+		document.getElementById(idScrewWeight).value) {
+		document.getElementById(idOneScrewWeight).innerHTML = numberFormat.format(document.getElementById(idScrewWeight).value / document.getElementById(nScrews));
+		document.getElementById(idTotalPrice).innerHTML = numberFormat.format(document.getElementById(nScrews).value * document.getElementById(idScrewPrice));
+	}
+}
