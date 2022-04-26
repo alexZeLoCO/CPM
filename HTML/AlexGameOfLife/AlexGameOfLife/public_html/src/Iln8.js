2b366cf64389e6c9fa09d324deed162c865e5247
Alex <alejandro.rod.lop@gmail.com>
2022-04-26 09:41:28 +0200

    Added Exam 2

--- HTML/AlexGameOfLife/AlexGameOfLife/public_html/src/Iln8.js
+++ HTML/AlexGameOfLife/AlexGameOfLife/public_html/src/Iln8.js
@@ -7,23 +7,28 @@ function getDate () {
 }
 
 //ReqI12
-function aUpdated (newA) {
+function aUpdated (newA, random) {
     //ReqI8
     document.getElementById('output').innerHTML = 
 					//ReqI13
-        '(A+B)/7 = ' + numberf.format((document.getElementById('newA').value+get('B'))/7);
+        '(A+B)/7 = ' + numberf.format((newA+get('B'))/7);
+	//DefI3 DefI4
+	document.getElementById('defOutput').innerHTML = numberf.format(Math.sqrt(newA)) + " [" + intf.format(Math.sqrt(newA)) + "]";
+
+	//DefI5
+	document.getElementById(random).innerHTML = intf.format(Math.random());
 }
 
 //ReqI13
 function bUpdated (newB) {
     //ReqI8
     document.getElementById('output').innerHTML = 
-        '(A+B)/7 = ' + numberf.format((document.getElementById('newB').value+get('A'))/7);
+        '(A+B)/7 = ' + numberf.format((newB+get('A'))/7);
     
     //ReqI9
 			document.getElementById('currencyOutput').innerHTML = 
 				//ReqI14
-        'C-B = ' + currencyf.format(get('C')-document.getElementById('newB').value);
+        'C-B = ' + currencyf.format(get('C')-newB);
 }
 
 //ReqI9
@@ -35,14 +40,24 @@ function cUpdated (newC) {
 //ReqI10
 function dateUpdated (newDate) {
     newDate = new Date (newDate);
-    console.log(newDate);
-	newDate.setYear((new Date()).getFullYear() - newDate.getFullYear());
-    console.log(newDate);
+    //console.log(newDate);
+	//newDate.setYear((new Date()).getFullYear() - newDate.getFullYear());
+    console.log(newDate.getFullYear());
+    //DefI2
+    if (newDate.getFullYear() > 2000) {
 	document.getElementById('dateShortOutput').innerHTML =
-				//ReqI15
+		//ReqI15
 		shortDatef.format(newDate);
-    document.getElementById('dateOutput').innerHTML = 
-				//ReqI16
-        longDatef.format(newDate);
+    	document.getElementById('dateOutput').innerHTML = 
+		//ReqI16
+        	shortDatef.format(newDate);
+    } else {
+	document.getElementById('dateShortOutput').innerHTML =
+		//ReqI15
+		longDatef.format(newDate);
+    	document.getElementById('dateOutput').innerHTML = 
+		//ReqI16
+        	longDatef.format(newDate);
+	}
 }
 
